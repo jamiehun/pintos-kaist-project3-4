5acfa80da268345651b3b1f8fec78c06d716fa71
jamiehun <jamiesunghun@gmail.com>
2022-12-03 14:14:21 +0000

    feat : implement supplemental page table

--- vm/vm.c
+++ vm/vm.c
@@ -3,6 +3,8 @@
 #include "threads/malloc.h"
 #include "vm/vm.h"
 #include "vm/inspect.h"
+#include "lib/kernel/hash.h"
+
 
 /* Initializes the virtual memory subsystem by invoking each subsystem's
  * intialize codes. */
@@ -60,23 +62,59 @@ err:
 	return false;
 }
 
-/* Find VA from spt and return page. On error, return NULL. */
-struct page *
-spt_find_page (struct supplemental_page_table *spt UNUSED, void *va UNUSED) {
-	struct page *page = NULL;
+
+/* spt에서 가상주소 va와 대응되는 페이지 구조체를 찾아서 반환함 
+   Find VA from spt and return page. On error, return NULL. */
+struct page *spt_find_page (struct supplemental_page_table *spt UNUSED, void *va UNUSED) {
+
 	/* TODO: Fill this function. */
 
-	return page;
+	/* 1) 새로 페이지를 할당받아서 페이지에 대한 va를 기존 va로 받음 
+	      대신, va를 저장할 때는 pg_round_down을 통해 시작점을 기준으로 설정 => va에 대한 페이지 준비 */
+	struct page *temp_page = (struct page *)malloc(sizeof(struct page)); // ??? page를 새로 할당받을 생각
+	temp_page->va = pg_round_down(va);
+
+
+	/* 2) spt 테이블에서 위에서 만든 hash_elem을 찾음(page 구조체에 hash_elem 추가)
+	      hash_table의 hash_elem이 곧 page이므로 저장해도 됨 */
+	struct hash_elem *temp_hash_elem = hash_find(&spt->table, &temp_page->hash_elem);
+
+	if (temp_hash_elem == NULL)
+		return NULL;
+
+    /* 3) temp_page의 경우 다 썼기 때문에 free하여 할당 해제함 */
+	free(temp_page);
+
+    /* 4) hash_elem != hash_entry를 통해 나온 hash_elem 
+		  hash_entry에서 나온 내용은 hash_elem, 곧 page라고 할 수 있음 */
+	return hash_entry(temp_hash_elem, struct page, hash_elem);
 }
 
-/* Insert PAGE into spt with validation. */
-bool
-spt_insert_page (struct supplemental_page_table *spt UNUSED,
-		struct page *page UNUSED) {
-	int succ = false;
+/*  주어진 보조테이블에 페이지 구조체를 삽입 (+ 존재하는지 검사)
+	Insert PAGE into spt with validation. */
+bool spt_insert_page (struct supplemental_page_table *spt UNUSED, struct page *page UNUSED) {
+	
 	/* TODO: Fill this function. */
+	
+	/* !!! 원본과 다름 !!!*/
+	/* === 원본 === */
+	// temp_elem 기준으로 판단
+	// if(temp_elem == NULL)
+	//	return true;
+	
+	/* === 나의 방식 === */
+	// 구현이유 : Gitbook 기준 
+	// This function should checks that the virtual address does not exist 
+	// in the given supplemental page table.
+	
+	struct page *temp_page = spt_find_page(spt, page->va);
 
-	return succ;
+	if(temp == NULL){
+		struct hash_elem *temp_elem = hash_insert(&spt->table, &page->hash_elem);
+		return true;
+	}
+
+	return false;
 }
 
 void
@@ -171,9 +209,12 @@ vm_do_claim_page (struct page *page) {
 	return swap_in (page, frame->kva);
 }
 
+/* 보조테이블을 초기화하는 함수 (해시테이블 자료 구조로 구현)
+   새로운 프로세스 시작하거나 do_fork로 자식 프로세스 생성될 때 위 함수 호출*/
 /* Initialize new supplemental page table */
 void
 supplemental_page_table_init (struct supplemental_page_table *spt UNUSED) {
+	hash_init(&spt->table, spt_hash, spt_less, NULL);
 }
 
 /* Copy supplemental page table from src to dst */
@@ -187,4 +228,24 @@ void
 supplemental_page_table_kill (struct supplemental_page_table *spt UNUSED) {
 	/* TODO: Destroy all the supplemental_page_table hold by thread and
 	 * TODO: writeback all the modified contents to the storage. */
+}
+
+/********** Project 3: Virtual Memory **********/
+/* hash_hash_func에 대한 결과값
+   Computes and returns the hash value for hash element E, given
+   auxiliary data AUX. */
+uint64_t spt_hash(const struct hash_elem *e, void *aux){
+	const struct page *temp_page = hash_entry(elem, struct page, hash_elem);
+	return hash_bytes(temp_page->va, sizeof(temp_page->va)); // temp_page->va 크기의 주소를 해시값으로 반환
+}
+
+/* hash_less_func에 대한 반환 값
+   Compares the value of two hash elements A and B, given
+   auxiliary data AUX.  Returns true if A is less than B, or
+   false if A is greater than or equal to B. */
+bool spt_less(const struct hash_elem *a, const struct hash_elem *b, void *aux){
+	const struct page *page_a = hash_entry(a, struct page, hash_elem);
+	const struct page *page_b = hash_entry(b, struct page, hash_elem);
+
+	return ((page_a->va < page_b->va) ? true : false);
 }
